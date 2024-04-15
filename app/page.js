93abcc0889d032de2f6cb150cc8370e8b78b6313
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2024-04-15 20:06:06 +0530

    feat: add a date & timestamp

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bold text-4xl">Welcome to Platform Engineering</h1>
+      <h1 className="bold text-4xl">Welcome to Platform Engineering 15/04/2024 20:05</h1>
     </main>
   );
 }
